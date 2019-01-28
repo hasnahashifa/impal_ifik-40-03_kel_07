937e3b18e6d8f22e61aa8ae0cdb48b47c073130a
candrasurya17 <46916618+candrasurya17@users.noreply.github.com>
2019-01-28 23:22:59 +0700

    Update pegawai pt 1 (2).sql

--- pegawai pt 1 (2).sql
+++ pegawai pt 1 (2).sql
@@ -10,3 +10,11 @@ CREATE TABLE Gaji2 (id_gaji Varchar2(10),status Varchar2(30),tanggal Varchar2(15
 insert into Gaji2 (id_gaji,status,tanggal,jumlah) values ('Px22','belum terklaim','1 desember','15000000');
 insert into Gaji2 (id_gaji,status,tanggal,jumlah) values ('Px33','belum terklaim','1 november','13000000');
 insert into Gaji2 (id_gaji,status,tanggal,jumlah) values ('Px36','belum terklaim','1 september','15000000');
+
+CREATE TABLE Absen (tanggal Varchar2(10),status Varchar2(20),NIP Varchar2(10), CONSTRAINT fk_absen FOREIGN KEY (NIP) REFERENCE Pegawai2 (NIP) , CONSTRAINT pk_absen PRIMARY KEY ( tanggal,NIP );
+
+insert into Absen (tanggal,status,NIP) values ('01/01/2019','belum absen','13011078');
+insert into Absen (tanggal,status,NIP) values ('01/01/2019','sudah absen','13011079');
+insert into Absen (tanggal,status,NIP) values ('01/01/2019','belum absen','13011068');
+insert into Absen (tanggal,status,NIP) values ('01/01/2019','sudah absen','13011077');
+insert into Absen (tanggal,status,NIP) values ('01/01/2019','belum absen','13011058');
