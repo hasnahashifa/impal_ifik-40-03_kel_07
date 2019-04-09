03798ae93ac837892076fb0d11adb992233e802c
dwinovSW <46889622+dwinovSW@users.noreply.github.com>
2019-04-09 20:27:44 +0700

    Create denda.java

---
+++ AndroidStudioProjects/HRlogin/app/src/main/java/com/example/hrlogin/denda.java
@@ -0,0 +1,48 @@
+package com.example.hrlogin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class denda extends AppCompatActivity {
+    TextView jumlah;
+    TextView kodedenda;
+    TextView tipedenda;
+    TextView niporNama;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_denda);
+    }
+
+    public void back(View view) {
+        Intent intent = new Intent(denda.this,mainMenuManager.class);
+        startActivity(intent);
+    }
+    public void input(View view) {
+        jumlah = findViewById(R.id.jumlahLembur);
+        kodedenda =findViewById(R.id.kodeL);
+        tipedenda = findViewById(R.id.keterangan);
+        niporNama =findViewById(R.id.nipnamapeg);
+
+        String jumlah1 = jumlah.getText().toString();
+        String Kodedenda1 = kodedenda.getText().toString();
+        String tipedenda1 = tipedenda.getText().toString();
+        String nipornama1 = niporNama.getText().toString();
+        if(isnull(jumlah1, Kodedenda1, tipedenda1, nipornama1)) {
+            Intent intent = new Intent(denda.this, mainMenuManager.class);
+            startActivity(intent);
+        }else{
+
+        }
+    }
+    public static Boolean isnull(String jumlah, String Kodedenda, String tipedenda, String nipOrnama ){
+        if((jumlah.equals("")||Kodedenda.equals(""))||(tipedenda.equals("")||nipOrnama.equals(""))){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
