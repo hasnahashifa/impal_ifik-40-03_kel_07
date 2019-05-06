e216f1d993245e94ff5b537eacd10f14e71983d0
dwinovSW <46889622+dwinovSW@users.noreply.github.com>
2019-05-06 23:27:58 +0700

    Update MainActivity.java

--- AndroidStudioProjects/HRlogin/app/src/main/java/com/example/hrlogin/MainActivity.java
+++ AndroidStudioProjects/HRlogin/app/src/main/java/com/example/hrlogin/MainActivity.java
@@ -1,17 +1,35 @@
 package com.example.hrlogin;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
     TextView username1;
     TextView pass1;
+    FirebaseDatabase db;
+    DatabaseReference dbRef;
+    public boolean DbUser;
+    public boolean Dbpass;
+    public boolean Dbjabatan;
+    public String user;
+    public String pass;
+    public String tampung1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        db = FirebaseDatabase.getInstance();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -32,26 +50,59 @@ public class MainActivity extends AppCompatActivity {
     public void Login(View view) {
         username1 = findViewById(R.id.username);
         pass1 =findViewById(R.id.pass);
+        user = username1.getText().toString();
+        pass = pass1.getText().toString();
+        dbRef = db.getReference().child("pegawai").child(user);
+        String tampung2;
+        Log.e("select err", "Login: "+dbRef.toString() );
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getKey().equals(user) && dataSnapshot.getValue()!=null) {
+                    DbUser = true;
+                    DataSnapshot dataSnapshot1= dataSnapshot.child("pass");
+                    Log.e("datasnap", "onDataChange: "+dataSnapshot1.toString() );
+                    String tampung1=dataSnapshot1.getValue().toString();
+                    if(pass.equals(tampung1)){
+                        Dbpass=true;
+                        dataSnapshot1= dataSnapshot.child("jabatan");
+                        tampung1 = dataSnapshot1.getValue().toString();
+                        if(("manager").equals(tampung1)){
+                            Dbjabatan=true;
+                        }
+                    }
 
-        String user = username1.getText().toString();
-        String pass = pass1.getText().toString();
-        if(Notnull(user,pass)==false){
-            Intent intent1 = new Intent(MainActivity.this,mainmenuPegawai.class);
-            Intent intent = new Intent(MainActivity.this,mainMenuManager.class);
-            if(konfirm(pass,user)==true){
-                startActivity(intent1);
-            }else{
-                startActivity(intent);
+                }
+                Log.e("show err" ,"onDataChange: "+dataSnapshot.toString() );
+                if( DbUser && Dbpass && Dbjabatan){
+                    Intent intent = new Intent(MainActivity.this,mainMenuManager.class);
+                    startActivity(intent);
+                } else if(DbUser && Dbpass ){
+                    Intent intent =new Intent(MainActivity.this,mainmenuPegawai.class);
+                    startActivity(intent);
+                }else{
+                    Intent intent =new Intent(MainActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+
             }
-        }
-    }
-    public static Boolean konfirm(String password, String username){
 
-        if(password.equals("2")&& username.equals("1")){
-            return true;
-        }else {
-           return false;
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        if(Notnull(user,pass)){
+
+
 
         }
+
+
+        }
+
     }
-}
+
+
+
