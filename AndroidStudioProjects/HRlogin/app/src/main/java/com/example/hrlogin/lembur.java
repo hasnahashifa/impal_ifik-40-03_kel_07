3848455ebad78042fd81a2fb266054f874d285f8
RiriRiyaddulloh24 <46895768+RiriRiyaddulloh24@users.noreply.github.com>
2019-05-12 20:31:47 +0700

    Update lembur.java

--- AndroidStudioProjects/HRlogin/app/src/main/java/com/example/hrlogin/lembur.java
+++ AndroidStudioProjects/HRlogin/app/src/main/java/com/example/hrlogin/lembur.java
@@ -3,8 +3,12 @@ package com.example.hrlogin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class lembur extends AppCompatActivity {
     TextView jumlah;
@@ -12,8 +16,13 @@ public class lembur extends AppCompatActivity {
     TextView tipeLembur;
     TextView niporNama;
 
+    FirebaseDatabase db;
+    DatabaseReference dbRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        db = FirebaseDatabase.getInstance();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lembur);
     }
@@ -25,8 +34,10 @@ public class lembur extends AppCompatActivity {
     public void input(View view) {
         jumlah = findViewById(R.id.jumlahLembur);
         kodeLembur =findViewById(R.id.kodeLembur);
-        tipeLembur = findViewById(R.id.kodeL);
+        tipeLembur = findViewById(R.id.TipeL);
         niporNama =findViewById(R.id.nipnamapeg);
+        dbRef= db.getReference().child("Lembur");
+        Log.e("select err", "Login: "+dbRef.toString() );
 
         String jumlah1 = jumlah.getText().toString();
         String Kodelembur1 = kodeLembur.getText().toString();
@@ -35,13 +46,20 @@ public class lembur extends AppCompatActivity {
         if(isnull(jumlah1,Kodelembur1,tipeLembur1,nipornama1)==true) {
             Intent intent = new Intent(lembur.this, mainMenuManager.class);
             startActivity(intent);
+            String key = dbRef.push().getKey();
+            dbRef.child(Kodelembur1);
+            dbRef.child(Kodelembur1).child("namaornip").setValue(nipornama1);
+            dbRef.child(Kodelembur1).child("jumlah").setValue(jumlah1);
+            dbRef.child(Kodelembur1).child("tipe").setValue(tipeLembur1);
+        }else{
+
         }
     }
-    public static Boolean isnull (String jumlah, String Kodelembur, String tipelembur, String nipOrnama ){
-        if((jumlah.equals("")||Kodelembur.equals(""))||(tipelembur.equals("")||nipOrnama.equals(""))){
-            return true;
-        }else {
+    public static Boolean isnull (String jumlah, String Kodelembur, String tipelembur, String nipornama ){
+        if((jumlah.equals("")||Kodelembur.equals(""))||(tipelembur.equals("")||nipornama.equals(""))){
             return false;
+        }else {
+            return true;
         }
     }
 }
