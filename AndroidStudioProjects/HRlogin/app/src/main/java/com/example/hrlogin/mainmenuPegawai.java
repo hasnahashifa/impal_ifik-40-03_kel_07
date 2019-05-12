4b3df971fda55c6c1bfb58de01431ed65b6b8751
dwinovSW <46889622+dwinovSW@users.noreply.github.com>
2019-05-12 22:13:43 +0700

    Update mainmenuPegawai.java

--- AndroidStudioProjects/HRlogin/app/src/main/java/com/example/hrlogin/mainmenuPegawai.java
+++ AndroidStudioProjects/HRlogin/app/src/main/java/com/example/hrlogin/mainmenuPegawai.java
@@ -3,10 +3,17 @@ package com.example.hrlogin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class mainmenuPegawai extends AppCompatActivity {
-
+    FirebaseDatabase db;
+    DatabaseReference dbRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +31,17 @@ public class mainmenuPegawai extends AppCompatActivity {
         startActivity(intent);
     }
     public void View(View view) {
+        Intent intent1 = getIntent();
+        Bundle extras = intent1.getExtras();
+        String data;
+        if(extras != null){
+            data= extras.getString("User");
+            dbRef= db.getReference().child("absen");
+            Log.e("select err", "Login: "+dbRef.toString() );
+            String date = "12 agustus 2019";
+            dbRef.child(data).child(date).setValue("hadir");
+        }
+
         Intent intent = new Intent(mainmenuPegawai.this,viewPresensi.class);
         startActivity(intent);
     }
