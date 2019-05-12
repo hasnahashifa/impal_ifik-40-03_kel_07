e9d7a6e610066f8a96d91411a37588940a5cbb65
dwinovSW <46889622+dwinovSW@users.noreply.github.com>
2019-05-12 23:25:36 +0700

    Update viewGaji.java

--- AndroidStudioProjects/HRlogin/app/src/main/java/com/example/hrlogin/viewGaji.java
+++ AndroidStudioProjects/HRlogin/app/src/main/java/com/example/hrlogin/viewGaji.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+
 
 public class viewGaji extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_gaji);
+
     }
 
     public void back(View view) {
