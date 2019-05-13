da1213fde188acfd58af0f091c84e6a1935bc3d0
dwinovSW <46889622+dwinovSW@users.noreply.github.com>
2019-05-13 20:44:09 +0700

    Update viewGaji.java

--- AndroidStudioProjects/HRlogin/app/src/main/java/com/example/hrlogin/viewGaji.java
+++ AndroidStudioProjects/HRlogin/app/src/main/java/com/example/hrlogin/viewGaji.java
@@ -1,25 +1,70 @@
 package com.example.hrlogin;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 
 public class viewGaji extends AppCompatActivity {
+    FirebaseDatabase db;
+    DatabaseReference dbRef;
+    public String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        db = FirebaseDatabase.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_gaji);
 
+
+
+
     }
+
+
 
     public void back(View view) {
         Intent intent =new Intent(viewGaji.this,mainmenuPegawai.class);
         startActivity(intent);
+    }
+    public void show(View view) {
+        Intent intent1 = getIntent();
+        final TextView Gaji= findViewById(R.id.gaji);
+        final TextView denda= findViewById(R.id.denda);
+        final TextView lembur= findViewById(R.id.lembur);
+
+
+
+
+        dbRef=db.getReference().child("Gaji").child("1B23A");
+        Log.e("bayarerr", "bayar: " + dbRef.getKey());
+
+            dbRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String data1 = dataSnapshot.child("jumlah").getValue().toString();
+                    Gaji.setText(data1);
+
+
+                }
+
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
     }
 }
