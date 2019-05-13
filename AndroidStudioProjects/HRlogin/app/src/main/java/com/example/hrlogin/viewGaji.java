ffcf4b70fcfb84c57eac58af7fdfd70789421ce8
dwinovSW <46889622+dwinovSW@users.noreply.github.com>
2019-05-13 23:55:33 +0700

    Update viewGaji.java

--- AndroidStudioProjects/HRlogin/app/src/main/java/com/example/hrlogin/viewGaji.java
+++ AndroidStudioProjects/HRlogin/app/src/main/java/com/example/hrlogin/viewGaji.java
@@ -19,6 +19,7 @@ import com.google.firebase.database.ValueEventListener;
 public class viewGaji extends AppCompatActivity {
     FirebaseDatabase db;
     DatabaseReference dbRef;
+
     public String data;
 
     @Override
@@ -32,6 +33,85 @@ public class viewGaji extends AppCompatActivity {
 
     }
 
+    public void  gajishow(String data, final TextView view){
+        DatabaseReference Dbref =db.getReference().child("Gaji").child(data);
+        Dbref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                    String data1 = dataSnapshot.child("jumlah").getValue().toString();
+
+                    view.setText(data1);
+
+
+
+
+
+
+
+
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        }
+    public void  dendashow(String data, final TextView view){
+        DatabaseReference Dbref =db.getReference().child("Denda").child(data);
+        Dbref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String data1 = dataSnapshot.child("jumlah").getValue().toString();
+
+                view.setText(data1);
+
+
+
+
+
+
+
+
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+    public void  lemburshow(String data, final TextView view){
+        DatabaseReference Dbref =db.getReference().child("Lembur").child(data);
+        Dbref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String data1 = dataSnapshot.child("jumlah").getValue().toString();
+
+                view.setText(data1);
+
+
+
+
+
+
+
+
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
 
 
     public void back(View view) {
@@ -39,6 +119,18 @@ public class viewGaji extends AppCompatActivity {
         startActivity(intent);
     }
     public void show(View view) {
+        final TextView gaji=findViewById(R.id.gaji);
+        final TextView denda=findViewById(R.id.denda);
+        final TextView lembur=findViewById(R.id.lembur);
+        Intent intent1 = getIntent();
+        Bundle extras = intent1.getExtras();
+        String data;
+        if(extras != null){
+            data= extras.getString("User");
+            gajishow(data,gaji);
+            dendashow(data,denda);
+            lemburshow(data,lembur);
+        }
 
 
     }
